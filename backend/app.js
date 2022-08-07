14161a6d8c1825bf94bbfe254e4dddda7891fccb
jonp2020 <jon.parnham@gmail.com>
2022-08-07 16:41:56 +0100

    Add db config to connect to mongo database

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { default: mongoose } = require("mongoose");
 const app = express();
 const { handleInvalidPath } = require("./errors");
 
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 const app = require("./app.js");
 const dotenv = require("dotenv").config();
+const connectDB = require("./config/db");
 const port = process.env.PORT || 4000;
+
+connectDB();
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
