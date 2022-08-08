17cbe9ff9abccac36238d9ccb5e283e4b23863fe
jonp2020 <jon.parnham@gmail.com>
2022-08-08 12:34:49 +0100

    Add images for home banner

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,11 +1,14 @@
 <template>
   <nav class="nav-bar">
-    <router-link to="/">Golden Shoe</router-link>
-    <router-link to="/">Home</router-link>
+    <router-link to="/">
+      <div class="brand_name_text">Golden <br />Shoe</div>
+    </router-link>
     <router-link to="/womens">Womens</router-link>
     <router-link to="/mens">Mens</router-link>
     <router-link to="/kids">Kids</router-link>
-    <router-link to="/cart">Shopping Cart</router-link>
+    <router-link to="/cart"
+      ><i class="fa-solid fa-cart-shopping"></i
+    ></router-link>
   </nav>
   <router-view />
 </template>
@@ -33,5 +36,13 @@ nav a {
 
 nav a.router-link-exact-active {
   color: #42b983;
+}
+
+.brand_name_text {
+  text-align: left;
+  font-style: italic;
+  color: orange;
+  font-weight: 900;
+  font-size: 1.5rem;
 }
 </style>
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,5 +1,19 @@
 <template>
-  <div class="home">Home View</div>
+  <div class="home">
+    <div>
+      <img
+        class="header-banner-img"
+        src="../assets/walking-large.jpg"
+        alt="Couple dressed in wedding clothes, wearing trainers"
+      />
+      <div class="header-banner-message-container">
+        <p class="header-banner-text">
+          Good shoes <br />take you <br />good places
+        </p>
+        <button class="shop-cta-btn">Shop now</button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -13,3 +27,39 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Calligraffitti&display=swap");
+.home {
+  position: relative;
+  max-width: 1500px;
+  margin: 0 auto;
+}
+.header-banner-img {
+  width: 100%;
+  height: auto;
+}
+
+.header-banner-message-container {
+  position: absolute;
+  top: 10%;
+  left: 20%;
+}
+.shop-cta-btn {
+  text-transform: uppercase;
+  font-size: 2rem;
+  border: 1px solid grey;
+  background-color: #000;
+  color: #fff;
+  padding: 0.5rem 2rem;
+  margin-top: 4rem;
+  cursor: pointer;
+}
+
+.header-banner-text {
+  font-family: "Calligraffitti", cursive;
+  font-size: 3rem;
+  font-weight: 500;
+  transform: rotate(-20deg);
+}
+</style>
