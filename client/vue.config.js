d3337ed2a296847d376cce7e9b39c114519cb10f
jonp2020 <jon.parnham@gmail.com>
2022-08-14 14:46:10 +0100

    Add public path name

--- client/vue.config.js
+++ client/vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("@vue/cli-service");
 const path = require("path");
 module.exports = defineConfig({
   transpileDependencies: true,
-  outputDir: path.resolve(__dirname, "..backend/public"),
+  outputDir: path.resolve(__dirname, "../backend/public"),
   devServer: {
     proxy: {
       "/api": {
