9681331de6c83178fe7edb8715f8880fdb49a16f
jonp2020 <jon.parnham@gmail.com>
2022-08-14 15:58:17 +0100

    add forward slash

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ connectDB();
 
 // Handle production
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(`${__dirname}/public`));
+  app.use(express.static(`${__dirname}/public/`));
 
   app.get(/.*/, (req, res) => res.sendFile(__dirname + "/public/index.html"));
 }
