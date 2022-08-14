33cefec548219436fa9fc2c7f11efe95df27e7fa
jonp2020 <jon.parnham@gmail.com>
2022-08-14 15:29:20 +0100

    handle SPA

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,8 @@ connectDB();
 // Handle production
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(`${__dirname}/public`));
+
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + "/public/index.html"));
 }
 
 app.listen(port, () => {
